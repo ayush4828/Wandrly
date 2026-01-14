f163ff5ad560105bd0fc514d76c1bed43301150c
ayush4828 <ayushpatel11486@gmail.com>
2026-01-14 22:50:42 +0530

    new and create  route successfully implemented

--- app.js
+++ app.js
@@ -47,12 +47,31 @@ app.get("/listings" , async(req ,res)=>{
     res.render("listings/index.ejs" , {allListings});
 })
 
+//new Route
+app.get("/listings/new" , (req,res)=>{
+res.render("listings/new.ejs");
+})
+
+//create Route
+
+app.post("/listings" , async (req,res)=>{
+    // let {title,description,price,location,country} = req.body;
+    // let listing = new Listing({title:title, description:description,image:image,price:price,location:location,country:country})
+    //await listing.save()
+    let listing = req.body.listing;;
+    // console.log(listing);
+    const newListing = new Listing(listing);
+    await newListing.save()
+    res.redirect("/listings");
+})
+
 //Show Route
 app.get("/listings/:id" , async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing})
 })
+
 app.listen(8080,()=>{
     console.log("server is listning on 8080")
 })
