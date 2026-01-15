1742b20be72b729a5e4d719bb55398763746fec5
ayush4828 <ayushpatel11486@gmail.com>
2026-01-15 22:56:22 +0530

    delete route added

--- app.js
+++ app.js
@@ -67,6 +67,14 @@ app.put("/listings/:id" , async (req,res)=>{
    res.redirect(`/listings/${id}`);
 })
 
+//delete route
+
+app.delete("/listings/:id" , async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
 app.listen(8080,()=>{
     console.log("server is listning on 8080")
 })
