15e22abcaf502dc9cdcd4d8a6c81c99b27654e10
ayush4828 <ayushpatel11486@gmail.com>
2026-01-16 10:12:58 +0530

    know aboout new package named ejs-mate for layouts

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require('method-override');
 const Listing = require("./models/listing.js")
+const ejsMate = require("ejs-mate")
 
 app.set("views" , path.join(__dirname,"views"))
 app.set("view engine" , "ejs")
-app.use(express.static(path.join(__dirname , "public")))
+app.use(express.static(path.join(__dirname , "/public")))
 app.use(express.urlencoded({ extended:true }))
 app.use(methodOverride('_method'))
-
+app.engine('ejs', ejsMate);
 
 main().then((res)=>{console.log("connection successful")}).catch(err => console.log(err));
 
