b9aa700ea70da3f6b40c7c49ca4fdf6831ad74b0
ayush4828 <ayushpatel11486@gmail.com>
2026-03-15 10:20:41 +0530

    adding passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const Session = require("express-session")
 const flash = require("connect-flash")
+const passport = require("passport")
+const User = require("./models/user.js")
+
 
 const listings = require("./routes/listing.js")
 const reviews = require("./routes/review.js");
@@ -45,6 +48,14 @@ const sessionOptions = {secret:"mysupersecretcodestring" ,
 
 app.use(Session(sessionOptions))
 app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
