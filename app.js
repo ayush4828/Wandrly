13d69a218324d0c242ce5157331bc626548afe8e
ayush4828 <ayushpatel11486@gmail.com>
2026-04-03 23:10:38 +0530

    Wonderlyyy Completed !!

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ app.use((req,res,next)=>{
 
 // 
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter)
 app.use("/",userRouter)
@@ -97,6 +101,7 @@ app.use((err,req,res,next)=>{
     let { statusCode = 500 , message = "something went wrong" } = err; 
     res.status(statusCode).render("error.ejs",{message})
 })
-app.listen(8080,()=>{
-    console.log("server is listning on 8080")
+const port = process.env.PORT || 8080;
+app.listen(port,()=>{
+    console.log(`server is listening on ${port}`)
 })
